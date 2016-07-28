b9aa8a9fd5ca31ace22e3608e4c7a5b9f76ec217
andig <cpuidle@gmx.de>
2016-07-28 15:09:25 +0200

    Better example with options

--- example.js
+++ example.js
@@ -8,29 +8,36 @@
 
 var fritz = require('./smartfritz.js');
 
-var username = DEFINE HERE;
-var password = DEFINE HERE;
+// NOTE: options only needs to be passed to each fritz function call if the Fritz!Box is not
+//       reachable at http://fritz.box
+
+var options = {
+    // "url": URL
+};
+
+var username = username;
+var password = password;
 
 
-fritz.getSessionID(username, password).then(function(sid) {
+fritz.getSessionID(username, password, options).then(function(sid) {
     console.log("SID: " + sid);
 
     // display switch information
-    fritz.getSwitchList(sid).then(function(switches) {
+    fritz.getSwitchList(sid, options).then(function(switches) {
         console.log("Switches: " + switches);
 
         if (switches.length) {
-            fritz.getSwitchName(sid, switches[0]).then(function(name) {
+            fritz.getSwitchName(sid, switches[0], options).then(function(name) {
                 console.log("Switch name [" + switches[0] + "]: " + name);
 
-                fritz.getSwitchPresence(sid, switches[0]).then(function(presence) {
+                fritz.getSwitchPresence(sid, switches[0], options).then(function(presence) {
                     console.log("Switch presence [" + switches[0] + "]: " + presence);
 
-                    fritz.getSwitchState(sid, switches[0]).then(function(state) {
+                    fritz.getSwitchState(sid, switches[0], options).then(function(state) {
                         console.log("Switch state [" + switches[0] + "]: " + state);
                     });
 
-                    fritz.getTemperature(sid, switches[0]).then(function(temp) {
+                    fritz.getTemperature(sid, switches[0], options).then(function(temp) {
                         console.log("Switch temperature [" + switches[0] + "]: " + temp + "°C");
                     });
                 });
@@ -39,21 +46,24 @@ fritz.getSessionID(username, password).then(function(sid) {
     });
 
     // display thermostat information
-    fritz.getThermostatList(sid).then(function(thermostats) {
+    fritz.getThermostatList(sid, options).then(function(thermostats) {
         console.log("Thermostats: " + thermostats);
 
         if (thermostats.length) {
-            fritz.getTemperature(sid, thermostats[0]).then(function(temp) {
+            fritz.getTemperature(sid, thermostats[0], options).then(function(temp) {
                 console.log("Thermostat temperature [" + thermostats[0] + "]: " + temp + '°C');
             });
 
-            fritz.getTempTarget(sid, thermostats[0]).then(function(temp) {
+            fritz.getTempTarget(sid, thermostats[0], options).then(function(temp) {
                 console.log("Get Target temperature [" + thermostats[0] + "]: " + temp + '°C');
             });
 
-            fritz.setTempTarget(sid, thermostats[0], 22.0).then(function(temp) {
+            fritz.setTempTarget(sid, thermostats[0], 22.0, options).then(function(temp) {
                 console.log("Set Target temperature [" + thermostats[0] + "]: " + temp + '°C');
             });
         }
     });
+})
+.catch(function(error) {
+    console.error(error);
 });
